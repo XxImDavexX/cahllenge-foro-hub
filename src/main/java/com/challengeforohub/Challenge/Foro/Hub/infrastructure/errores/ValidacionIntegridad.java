4f6145490a02167d09d9366fcb8eb2d7b21d3845
XxImDavexX <david411dark@gmail.com>
2024-07-11 18:00:15 -0600

    Creamos una clase para tratar los errores de Validacion Integridad

---
+++ src/main/java/com/challengeforohub/Challenge/Foro/Hub/infrastructure/errores/ValidacionIntegridad.java
@@ -0,0 +1,8 @@
+package com.challengeforohub.Challenge.Foro.Hub.infrastructure.errores;
+
+public class ValidacionIntegridad extends RuntimeException {
+
+    public ValidacionIntegridad(String s){
+        super(s);
+    }
+}

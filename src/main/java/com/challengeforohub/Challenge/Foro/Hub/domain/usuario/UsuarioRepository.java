95843fc7c267cecd1bb67362b3d2b0aa5ba93eaf
XxImDavexX <david411dark@gmail.com>
2024-07-11 22:23:00 -0600

    Se la funicon para encontrar el usuario medieante email

--- src/main/java/com/challengeforohub/Challenge/Foro/Hub/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/challengeforohub/Challenge/Foro/Hub/domain/usuario/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package com.challengeforohub.Challenge.Foro.Hub.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByEmail(String email);
 }

0c2f15bf6c19af7c2807505c70786c4f09d5e35a
XxImDavexX <david411dark@gmail.com>
2024-07-11 22:20:02 -0600

    Creammos el DTO con los valores que se usaran para identificar un usuario

---
+++ src/main/java/com/challengeforohub/Challenge/Foro/Hub/domain/usuario/DatosAutenticacionUsuario.java
@@ -0,0 +1,5 @@
+package com.challengeforohub.Challenge.Foro.Hub.domain.usuario;
+
+public record DatosAutenticacionUsuario( String email,
+                                         String password) {
+}
